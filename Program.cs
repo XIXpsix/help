a37a7bc5ecb8ef5a5effbab8c71da41fdd981b9c
lebed <lebed@LAPTOP-AHF84HUF>
2025-06-26 19:56:41 +0300

    .

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,16 @@ namespace help
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Point a = new Point(0, 0, "A");
+            Point b = new Point(4, 0, "B");
+            Point c = new Point(4, 3, "C");
+            Figure triangle = new Figure(a, b, c);
+            triangle.PerimeterCalculator();
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
         }
     }
 }
