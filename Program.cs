ba3aab91916d79b17839a9a4beada38335051b0a
lebed <lebed@LAPTOP-AHF84HUF>
2025-06-26 21:41:32 +0300

    .

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +8,31 @@ namespace help
 {
     internal class Program
     {
+        static void ClassTaker(MyClass myClass)
+        {
+            myClass.change = "изменено";
+        }
+
+        static void StruktTaker(MyStruct myStruct)
+        {
+            myStruct.change = "изменено";
+        }
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            MyClass classObj = new MyClass();
+            MyStruct structObj = new MyStruct();
+
+            classObj.change = "не изменено";
+            structObj.change = "не изменено";
+
+            ClassTaker(classObj);
+            StruktTaker(structObj);
+
+            Console.WriteLine("classObj.change = {0}", classObj.change);   // изменено
+            Console.WriteLine("structObj.change = {0}", structObj.change); // не изменено
         }
     }
 }
