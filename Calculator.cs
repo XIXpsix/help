7d2e47879c6e2144cdb7fe355666fa65a1cd8486
lebed <lebed@LAPTOP-AHF84HUF>
2025-06-26 21:38:55 +0300

    .

---
+++ Calculator.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace help
+{
+    public static class Calculator
+    {
+        public static double Add(double a, double b) => a + b;
+        public static double Subtract(double a, double b) => a - b;
+        public static double Multiply(double a, double b) => a * b;
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Деление на ноль невозможно.");
+            return a / b;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,15 @@ namespace help
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            double a = 10;
+            double b = 5;
+
+            Console.WriteLine($"Сложение: {a} + {b} = {Calculator.Add(a, b)}");
+            Console.WriteLine($"Вычитание: {a} - {b} = {Calculator.Subtract(a, b)}");
+            Console.WriteLine($"Умножение: {a} * {b} = {Calculator.Multiply(a, b)}");
+            Console.WriteLine($"Деление: {a} / {b} = {Calculator.Divide(a, b)}");
         }
     }
 }
