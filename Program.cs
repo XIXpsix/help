3457e39efaf2ea05b9e719fc85bb1fd566300d74
lebed <lebed@LAPTOP-AHF84HUF>
2025-06-26 19:47:16 +0300

    .

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,20 @@ namespace help
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Введите длину первой стороны прямоугольника: ");
+            double side1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите длину второй стороны прямоугольника: ");
+            double side2 = double.Parse(Console.ReadLine());
+
+            Rectangle rect = new Rectangle(side1, side2);
+
+            Console.WriteLine($"Площадь прямоугольника: {rect.Area}");
+            Console.WriteLine($"Периметр прямоугольника: {rect.Perimeter}");
+
+            Console.ReadKey();
         }
     }
 }
