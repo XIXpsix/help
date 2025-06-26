a2f6f7d1be8b64dbc67820bf677564f19506ccf6
lebed <lebed@LAPTOP-AHF84HUF>
2025-06-26 21:02:00 +0300

    .

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,33 @@ namespace help
     {
         static void Main(string[] args)
         {
+            const string proKey = "pro";
+            const string expKey = "exp";
+
+            Console.WriteLine("Введите ключ доступа (pro/exp), либо нажмите Enter для бесплатной версии:");
+            string key = Console.ReadLine();
+
+            DocumentWorker worker;
+
+            if (key == expKey)
+            {
+                worker = new ExpertDocumentWorker();
+            }
+            else if (key == proKey)
+            {
+                worker = new ProDocumentWorker();
+            }
+            else
+            {
+                worker = new DocumentWorker();
+            }
+
+            worker.OpenDocument();
+            worker.EditDocument();
+            worker.SaveDocument();
+
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
         }
     }
 }
