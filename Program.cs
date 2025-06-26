de513ab0f240681f5fc5ad153f0c15c5b2b5ff65
lebed <lebed@LAPTOP-AHF84HUF>
2025-06-26 21:14:26 +0300

    .

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace help
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
             const string proKey = "pro";
             const string expKey = "exp";
 
