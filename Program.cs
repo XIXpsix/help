d1135775ce1ef29e8e645d02925157e12c501174
lebed <lebed@LAPTOP-AHF84HUF>
2025-06-26 21:13:35 +0300

    .

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace help
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
             Console.WriteLine("Введите формат документа (xml, txt, doc):");
             string format = Console.ReadLine();
 
