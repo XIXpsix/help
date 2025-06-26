8b1280cadd001282b6e2e91105abbb92145973a9
lebed <lebed@LAPTOP-AHF84HUF>
2025-06-26 22:04:51 +0300

    .

---
+++ Calculator.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace help
+{
+    internal class Calculator
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Sub(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Div(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException("Деление на ноль невозможно!");
+            return a / b;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,45 @@ namespace help
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                Console.Write("Введите первое число: ");
+                double a = double.Parse(Console.ReadLine());
+
+                Console.Write("Введите второе число: ");
+                double b = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Выберите операцию: +  -  *  /");
+                string op = Console.ReadLine();
+
+                double result = 0;
+                switch (op)
+                {
+                    case "+":
+                        result = Calculator.Add(a, b);
+                        break;
+                    case "-":
+                        result = Calculator.Sub(a, b);
+                        break;
+                    case "*":
+                        result = Calculator.Mul(a, b);
+                        break;
+                    case "/":
+                        result = Calculator.Div(a, b);
+                        break;
+                    default:
+                        throw new InvalidOperationException("Неизвестная операция!");
+                }
+
+                Console.WriteLine($"Результат: {result}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
         }
     }
 }
